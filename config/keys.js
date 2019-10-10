0173f8475609c79d7da65bbfd0ddb3871c91e1d2
El Ghazi Mouad <elghazi.mouad1@gmail.com>
2019-10-10 18:28:41 +0100

    debug 1

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,7 @@
 if (process.env.Node_ENV === 'production'){
+	console.log('proooduction', process.env.Node_ENV);
 	module.export = require('./prod');
 } else {
+	console.log('devvvv')
 	module.export = require('./dev');
 }
